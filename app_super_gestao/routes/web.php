822f1b9e700652469307728beffd4bcbc4fb8960
ErickGX <ericksaopaulino1@hotmail.com>
2024-06-17 06:06:18 -0300

    Rotas/Controllers/Views bases implementadas, aprofundando rotas e valores padrões.

---
+++ app_super_gestao/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+Route::get('/sobrenos', [SobreNosController::class, 'sobreNos']);
+
+
+//nome , categoria, assunto, mensagemm
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}', 
+function(string $nome, string $categoria, string $assunto, string $mensagem = 'Mensagem não informada'){
+        echo 'Estamos aqui: '.$nome.' - '.$categoria.' - '.$assunto.' - '.$mensagem;
+});
+
+
