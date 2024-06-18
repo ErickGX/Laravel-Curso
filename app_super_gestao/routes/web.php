414234312207eb8b6c8c62e2fe234e12b4061268
ErickGX <ericksaopaulino1@hotmail.com>
2024-06-18 00:57:00 -0300

    Aplicando expressões regulares nos parametros das rotas.

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -10,12 +10,40 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/contato', [ContatoController::class, 'contato']);
 
 Route::get('/sobrenos', [SobreNosController::class, 'sobreNos']);
-//tESTANDO COMMIT
+
+
+
+//aplicando Expressoes regulares nas rotas
+Route::get(
+        '/contato/{nome?}/{categoria_id?}', 
+function(
+        string $nome = 'Desconhecido', 
+        int $categoria_id = 1, //  1 = 'informação
+        ){
+        echo 'Estamos aqui: '.$nome.' - '.$categoria_id;
+}
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); //no item cat id recebe apenas do 0 a 9 e pelo menos 1 numero
+
+
+
+
+
 
 //nome , categoria, assunto, mensagemm
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}', 
-function(string $nome, string $categoria, string $assunto, string $mensagem = 'Mensagem não informada'){
+//valores opcionais são passados da direita para a esquerda 
+
+/*
+Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
+function(
+        string $nome = 'Desconhecido', 
+        string $categoria = 'informação',
+         string $assunto = 'Contato', 
+         string $mensagem = 'Mensagem não informada')
+         {
         echo 'Estamos aqui: '.$nome.' - '.$categoria.' - '.$assunto.' - '.$mensagem;
-});
+}); 
+*/
+
+
 
 
