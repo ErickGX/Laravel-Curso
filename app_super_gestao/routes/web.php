be1c32f33ba451e481224ecb530f1a73d72e655e
ErickGX <ericksaopaulino1@hotmail.com>
2024-06-18 01:42:38 -0300

    Agrupamento e nomeação de rotas para melhor manutenção das routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -5,14 +5,25 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/login' , function(){ return 'login';})->name('site.login');
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+//names sao utilizados apenas dentro da logica da aplicacao, utilziando o helper  {{ route('site.index') }}, alterações nao refletem as rotas absolutas usando esse metodo
+//qualquer alteração na rota diretamente /contato , posso mudar para /qualquer coisa que o helper ainda continua apontando para a rota absoluta
 
-Route::get('/sobrenos', [SobreNosController::class, 'sobreNos']);
+//agrupamento de rotas a partir de um prefixo comum / separar partes da aplicacoes
+Route::prefix('/app')->group(function(){
+
+        Route::get('/clientes' , function(){ return 'clientes';})->name('app.clientes');
+        Route::get('/fornecedores' , function(){ return 'fornecedores';})->name('app.fornecedores');
+        Route::get('/produtos' , function(){ return 'produtos';})->name('app.produtos');
+});
 
 
 
+/*
 //aplicando Expressoes regulares nas rotas
 Route::get(
         '/contato/{nome?}/{categoria_id?}', 
@@ -25,14 +36,10 @@ function(
 )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); //no item cat id recebe apenas do 0 a 9 e pelo menos 1 numero
 
 
-
-
-
-
 //nome , categoria, assunto, mensagemm
 //valores opcionais são passados da direita para a esquerda 
 
-/*
+
 Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
 function(
         string $nome = 'Desconhecido', 
