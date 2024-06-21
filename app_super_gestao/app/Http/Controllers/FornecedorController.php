41c07ce50a11ffaed51051ae4de691ccd0958ece
ErickGX <ericksaopaulino1@hotmail.com>
2024-06-21 04:58:07 -0300

    Finalizando sintaxe basica blade e adicionando codigo das views principais

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -8,9 +8,14 @@ class FornecedorController extends Controller
 {
     public function index(){
         $fornecedores = [
-          0=> ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj'=>null],
-          1=> ['nome' => 'Fornecedor 1', 'status' => 'S']
+          0=> ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj'=>'', 'ddd'=>'11', 'telefone' => '0000-0000'],
+          1=> ['nome' => 'Fornecedor 2', 'status' => 'S', 'cnpj'=>'000.000.000/0000-00', 'ddd' => '21', 'telefone' => '0000-0000'],
+          2=> ['nome' => 'Fornecedor 3', 'status' => 'N', 'cnpj'=>null, 'ddd' => '32', 'telefone' => '0000-0000']
         ];
+
+        
+
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,7 +3,7 @@
 {{-- Comentario que sera descartado pelo blade --}}
 
 {{'TESTE LARAVAL'}}
-<br>
+<hr>
 @php
 /*
    if (isset($variavel){  //retorna true se a var estiver definida (isset($variavel)
@@ -39,19 +39,85 @@
     @endisset    
 @endisset --}}
 
-
-@isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome']}}
-        <br>
-    Status : {{$fornecedores[0]['status']}}
-        <br>    
-    CNPJ: {{$fornecedores[0]['cnpj'] ?? 'Dado não foi informado/preenchido'}} <!--Operador condional de valor default -->
-    <!-- 
-            $var testada nao estiver definida (isset)
+<!-- 
+            $var testada se nao estiver definida (isset)  condicao ?? valor default caso n atendida
                 ou
             $var testada possuir valor null (valores como 0, ' ' , false , nao serao considerados)
     -->
+
+{{-- @isset($fornecedores)
+@for ($i = 0; isset($fornecedores[$i]); $i++)
+    Fornecedor: {{ $fornecedores[$i]['nome']}}
+        <br>
+    Status : {{$fornecedores[$i]['status']}}
+        <br>    
+    CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'Dado não foi informado/preenchido'}} <!--Operador condional de valor default -->
+        <br>
+    Telefone: ({{$fornecedores[$i]['ddd'] ?? 'Estado não foi definido'}}) {{$fornecedores[$i]['telefone'] ?? 'Telefone não foi informado/preenchido'}}
+       <hr>     
+   @endfor
 @endisset
+
+
+@php $i = 0 @endphp
+@while (isset($fornecedores[$i]))
+
+    Fornecedor: {{ $fornecedores[$i]['nome']}}
+        <br>
+    Status : {{$fornecedores[$i]['status']}}
+        <br>    
+    CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'Dado não foi informado/preenchido'}} <!--Operador condional de valor default -->
+        <br>
+    Telefone: ({{$fornecedores[$i]['ddd'] ?? 'Estado não foi definido'}}) {{$fornecedores[$i]['telefone'] ?? 'Telefone não foi informado/preenchido'}}
+        <hr>     
+    @php $i++; @endphp
+@endwhile
+   --}}
+
+  
+@forelse($fornecedores as $indice => $fornecedor)
+    
+    Iteração atual {{$loop->iteration}} 
+    <br>
+   Fornecedor: {{ $fornecedor['nome']}}
+        <br>
+    Status : {{$fornecedor['status']}}
+        <br>    
+    CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não foi informado/preenchido'}} <!--Operador condional de valor default -->
+        <br>
+    Telefone: ({{$fornecedor['ddd'] ?? 'Estado não foi definido'}}) {{$fornecedor['telefone'] ?? 'Telefone não foi informado/preenchido'}}
+        <br>
+        @if ($loop->first)
+            {{-- This is the first iteration --}} primeira iteracao
+        @endif
+        
+        @if ($loop->last)
+            {{-- This is the last iteration --}}
+            Ultima iteracao do loop
+            total de registros : {{$loop->count}} 
+        @endif
+        
+    <hr>
+  
+     @empty   
+        Não existem fornedores cadastrados!!!
+@endforelse
+    {{-- @switch($fornecedores[0]['ddd'])
+        @case(11)
+            São Paulo - SP
+            @break
+        @case(21)
+            Rio de Bosteiro - RJ
+            @break
+        @case(32)
+            Juiz de Fora - MG
+            @break
+        @default
+            Estado não informado
+    @endswitch --}}
+
+
+
 
 
 
